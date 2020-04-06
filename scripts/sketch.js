de8be3ada08980ac9da5a0354600b2d13e50a57a
2019osbjoh <john.osborne151@gmail.com>
2020-04-06 11:56:26 +0000

    first commit

---
+++ scripts/sketch.js
@@ -0,0 +1,18 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitbutton = document.getElementById('submt');
+
+function buildQuiz() {
+	
+}
+
+function showResults() {
+
+}
+
+// Display quiz at start
+buildQuiz();
+
+// Once answers submitted, show results
+submitButton.addEventListener('click', showResults);
+
